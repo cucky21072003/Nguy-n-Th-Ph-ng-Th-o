1a14f276d451ca2ea60a4ca148c899c882082084
cucky21072003 <142881215+cucky21072003@users.noreply.github.com>
2023-10-31 13:32:42 +0700

    cucky21072003

--- Screen/Categories/index.js
+++ Screen/Categories/index.js
@@ -1,15 +1,42 @@
 import { View, Text,Image} from 'react-native'
 import React from 'react'
 import styles from './styles';
+import { Button, Icon } from 'react-native-paper';
 
 const Categories = () => {
-  const image = require('../Categories/img/avatar-Meo-buon-cute.jpg')
+  const image = require('../Categories/img/imgFlowers1.png')
      return (
     <View style={styles.body}>
         <Image
           source={image} style={styles.image123}
         />
-        <Text style={styles.text}>Em chưa làm xong bài ạ!!!</Text>
+
+        <View style={styles.content}>
+          <Text style={styles.textBigg}>Go for a walk and feed the dog</Text>
+        </View>
+
+        <View style={styles.textSug}>
+          <Text style={styles.content2}>
+          Leaving for a week, French Bulldog Wilfred needs help feeding twice a day
+          and walk from 26 to 30 September. I bought food, it will be easy.
+          </Text>
+        </View>
+
+        <View style={styles.textIcon}>
+          <Icon name="wallet" size={20} color="black"/>
+          <Text style={styles.contact}>Reward 34$</Text>
+        </View>
+
+        <View style={styles.textIcon}>
+          <Icon name="geo" size={20} color="black"/>
+          <Text style={styles.contact}>3 HERALD</Text>
+          <Text style={styles.textcontact}>400m from you</Text>
+        </View>
+
+        <View style={styles.textIcon}>
+          <Icon name="message" size={50} color="black"/>
+          
+        </View>
     </View>
   )
 }
--- Screen/Categories/styles.js
+++ Screen/Categories/styles.js
@@ -2,15 +2,23 @@ import { StyleSheet } from 'react-native'
 
 const styles = StyleSheet.create({
     image123:{
-        marginTop: 70,
         width: 400,
-        height: 250,
+        height: 400,
     },
 
-    text:{
-        fontSize: 14,
-        textAlign: 'center',
-        padding: 50,
+    textBigg:{
+        fontSize: 26, 
+    },
+    
+    content2:{
+        fontSize: 16,
+    },
+    textIcon:{
+        flexDirection:'row',
+    },
+    content:{
+        marginTop: 10,
+        marginBottom: 10,
     }
 })
 
